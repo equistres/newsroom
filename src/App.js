ddaca9d3837f4bcfd982abe44eefbbed0fccbb48
Soto <asoto@lanacion.com.ar>
2019-03-08 12:29:05 -0300

    paginado
    agrego paginado, nuevo feed US, arreglos visuales

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import GetNews from './containers/GetNews';
 
+const ARGNEWS = "https://newsapi.org/v2/top-headlines?country=ar&apiKey=91a3bc0b07184b7a8bf352ff162016cd";
+const USNEWS = "https://newsapi.org/v2/top-headlines?country=us&apiKey=91a3bc0b07184b7a8bf352ff162016cd";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          NEWSROOM
+          <img src="https://www.schneiderpr.com/wp-content/uploads/2015/12/SA-Newsroom-Logo.png" alt="newsroom"/>
         </header>
-        <GetNews />
+        <h1>LocalNews</h1>
+        <GetNews api={ARGNEWS}/>
+        <h1>US News</h1>
+        <GetNews api={USNEWS}/>
       </div>
     );
   }
--- src/containers/GetNews.js
+++ src/containers/GetNews.js
@@ -1,20 +1,26 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import News from '../components/News';
 
 
-const API_URL= 'https://newsapi.org/v2/top-headlines?country=ar&apiKey=91a3bc0b07184b7a8bf352ff162016cd'
 
-class GetNews extends PureComponent {
+class GetNews extends Component {
     constructor(props){
         super(props);
         this.state = {
           list : [],
           fetched : false,
-          loading : false
+          loading : false,
+          initialCount:0,
+          maxCount: 5,
+          Increment: 5
         };
-    }   
-    componentDidMount(){ 
-      fetch(API_URL)
+    }
+    updatePage = () => {
+        this.render()
+    }     
+    componentDidMount(){
+        debugger
+      fetch(this.props.api)
         .then(response => response.json())
         .then(data=>{
             this.setState({
@@ -23,13 +29,53 @@ class GetNews extends PureComponent {
               fetched : true
             });
           });
+    }
+
+    handleClickNext = (event) => {      
+        let inicio = this.state.initialCount+this.state.Increment;
+        let fin = this.state.maxCount+this.state.Increment;
+        if(inicio<=20 && fin<=20){
+          this.setState({
+            initialCount: inicio,
+            maxCount : fin
+          }, this.updatePage);
+        };
+    }
+
+    handleClickPrev = (event) => {      
+        let inicio = this.state.initialCount-this.state.Increment;
+        let fin = this.state.maxCount-this.state.Increment;
+        if(inicio>=0){
+          this.setState({
+            initialCount: inicio,
+            maxCount : fin
+          }, this.updatePage);
+        };
     }    
 
+
     render() {
+        //console.log(this.state.list.articles)
+        let items  = [];
+        let nextButton;
+        let PrevButton;
         const {fetched, loading} = this.state;
         let content;
         if(fetched){
-            content =  this.state.list.articles.map((item) => {
+            if(this.state.maxCount===20)
+            {
+                nextButton = "";
+            }else{
+                nextButton = <button onClick={this.handleClickNext}>Siguientes</button>;
+            }
+            if(this.state.initialCount===0)
+            {
+                PrevButton = "";
+            }else{
+                PrevButton = <button onClick={this.handleClickPrev}>Anteriores</button>;
+            }            
+            items = this.state.list.articles.slice(this.state.initialCount,this.state.maxCount);
+            content =  items.map((item) => {
                 return(<News item={item} key={item.publishedAt}/>)
             })
 
@@ -38,7 +84,13 @@ class GetNews extends PureComponent {
         }else{
             content = <div/>;
         }
-        return(<div className="row justify-content-center">{content}</div>)
+        return(
+        <div className="row justify-content-center mt-4">
+        {PrevButton}
+        {content}
+        {nextButton}
+        </div>
+        )
     }
 }
 
