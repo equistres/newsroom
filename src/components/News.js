58d589de09166856de28cee5b8937bda87dd1384
Alejandro Soto <equistres@gmail.com>
2019-03-08 00:33:52 -0300

    estilos
    agrego estilos

--- src/components/News.js
+++ src/components/News.js
@@ -4,8 +4,11 @@ import './News.css';
 const News = (item) => {
         return (
             <div className="column"> 
-                <div className="card mr-2" style={{width: '18rem', height:'600px'}}>                    
-                    <img src={item.item.urlToImage} className="card-img-top" alt="..." style={{width: '286px', height:'150px'}}/>
+                <div className="card mr-2" style={{width: '18rem', height:'600px'}}>
+                    <div className="container">                 
+                        <img src={item.item.urlToImage} className="card-img-top" alt="..." style={{width: '286px', height:'150px'}}/>
+                        <span className="badge badge-primary bottom-right">Via {item.item.source.name}</span> 
+                    </div> 
                     <div className="card-body">
                         <h6 className="card-title block-with-text">{item.item.title}</h6>
                         <p className="card-text block-with-description">{item.item.description}</p>
