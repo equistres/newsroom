26ecd62a75a44d9c760934ab699709a8aa54f4bb
Alejandro Soto <equistres@gmail.com>
2019-03-09 10:02:17 -0300

    Updates

--- precache-manifest.f1b136a6f64bb298fd99985f914da14d.js
+++ precache-manifest.f1b136a6f64bb298fd99985f914da14d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b43cdb8ed9e16324a1f0",
+    "revision": "4b13dbd02ed243917948",
     "url": "/newsroom/static/css/main.d63120dd.chunk.css"
   },
   {
-    "revision": "b43cdb8ed9e16324a1f0",
-    "url": "/newsroom/static/js/main.14a01867.chunk.js"
+    "revision": "4b13dbd02ed243917948",
+    "url": "/newsroom/static/js/main.9268983b.chunk.js"
   },
   {
     "revision": "be0126e1107a9e06f556",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/newsroom/static/js/2.10117d87.chunk.js"
   },
   {
-    "revision": "90bca73532b4d822f1b6a1394a5ef92a",
+    "revision": "71efed792a4e88462b761db7f2d93496",
     "url": "/newsroom/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/newsroom/precache-manifest.6a799a87ba79195be17330b5c1d1a8f3.js"
+  "/newsroom/precache-manifest.f1b136a6f64bb298fd99985f914da14d.js"
 );
 
 workbox.clientsClaim();
